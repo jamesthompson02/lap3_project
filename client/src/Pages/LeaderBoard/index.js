b3619d85b7664108b25b5c2278da8682d1fa5b2a
sam <sampurna.chapagainnn@gmail.com>
2022-08-15 17:15:02 +0100

    add leaderboard page with react-confetti settings

--- client/src/Pages/LeaderBoard/index.js
+++ client/src/Pages/LeaderBoard/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactConfetti from 'react-confetti';
 
 const LeaderBoard = () => {
+  const styles = {
+    textAlign: 'center',
+    listStylePosition: 'inside',
+  };
+  const [dimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  console.log(dimension);
   return (
-    <div>
-      <h1>LeaderBoard Page</h1>
+    <div style={styles}>
+      <ReactConfetti />
+      <h1>Final Scores</h1>
+      <br />
+      <h5>Here is your score:</h5>
+      <br />
+      <p>Jack: 9</p>
+      <h1>All time High Scores:</h1>
+      <ol>
+        <li>Jake: 9</li>
+        <li>Jack: 9</li>
+        <li>Jack: 9</li>
+        <li>Jack: 9</li>
+        <li>Jack: 9</li>
+        <li>Jack: 9</li>
+        <li>Jack: 9</li>
+      </ol>
     </div>
   );
 };
