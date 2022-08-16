c038c1c6aa66c23923f887031c6d06101e955339
HassanNaeem <hassannaeem288@gmail.com>
2022-08-16 14:12:43 +0100

    added the question button works fine

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Components/Footer';
-import NavBar from './Components/Routes';
-import * as Pages from './Pages';
+import QuizCategories from './Components/QuizCategories';
+
+
+
+
 
 
 const App=()=> {
   return (
-
+    
     <div className="App">
-      <NavBar />
-      <Footer />
-      <Routes>
-        <Route path="/" element={<Pages.Home />} />
-        <Route path="/about" element={<Pages.About />} />
-        <Route path="/quiz" element={<Pages.Quiz />} />
-        <Route path="/leaderboard" element={<Pages.LeaderBoard />} />
-      </Routes>
+      <h1>Quiz App</h1>
+      <QuizCategories/>
     </div>
   );
-
 }
-
 export default App;
