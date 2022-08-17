4c455061788d9c1cf66ae764b93c0c839a3f5220
sam <sampurna.chapagainnn@gmail.com>
2022-08-17 15:47:31 +0100

    display values form the backend

--- client/src/Pages/QuizCategories/index.js
+++ client/src/Pages/QuizCategories/index.js
@@ -1,57 +1,74 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import io from 'socket.io-client';
 import './QuizCategories.style.css';
 
 import { Answers, CardQuestion } from '../../Components';
 
 const QuizCategories = () => {
+  const serverEndpoint = 'https://lap3-project.herokuapp.com/';
   const [data, setData] = useState({});
   const [answers, setAnswers] = useState([]);
+  const [socket, setSocket] = useState(null);
+  // const [buttonColor, setbuttonColor] = useState('bg');
+  const username = 'testuser';
+  const roomid = 'testQuestions11';
+  // const fetchData = async () => {
+  //   const response = await axios.get(
+  //     'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple'
+  //   );
 
-  const fetchData = async () => {
-    const response = await axios.get(
-      'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple'
-    );
+  //   return { ...response.data.results[0] };
+  // };
 
-    return { ...response.data.results[0] };
+  // const grabData = async () => {
+  //   const data = await fetchData();
+
+  //   const joinedAnswers = [data.correct_answer, ...data.incorrect_answers];
+
+  //   setData(data);
+  //   setAnswers(joinedAnswers);
+  // };
+
+  // useEffect(() => {
+  //   grabData();
+  // }, []);
+
+  const connectRoom = () => {
+    // await new Promise((r) => setTimeout(r, 3000));
+    console.log(socket);
+    socket.emit('join-room', {
+      roomid,
+      username,
+    });
+    socket.emit('start-game', { roomid });
   };
-
-  const grabData = async () => {
-    const data = await fetchData();
-
-    const joinedAnswers = [data.correct_answer, ...data.incorrect_answers];
-    const shuffledAnswers = shuffle(joinedAnswers);
-
-    setData(data);
-    setAnswers(shuffledAnswers);
-  };
-
-  const shuffle = (array) => {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  };
-
   useEffect(() => {
-    grabData();
+    const newSocket = io(serverEndpoint);
+    //define all your socket events you'll expect to receive
+    newSocket.on('next-question', ({ nextQuestion }) => {
+      //Do something with the values
+      setData(nextQuestion);
+      setAnswers([
+        nextQuestion.correct_answer,
+        ...nextQuestion.incorrect_answers,
+      ]);
+      // setbuttonColor('bg');
+    });
+    setSocket(newSocket);
   }, []);
 
   const handleClick = (e) => {
+    // const currentClickedButton = e.target.closest('button');
+    // const choice = currentClickedButton.value;
+
+    // if (choice === data.correct_answer) {
+    //   currentClickedButton.className = 'bgGreen';
+    // } else {
+    //   currentClickedButton.className = 'bgRed';
+    // }
+    // currentClickedButton.className = buttonColor;
+
     const clickedButton = e.target.closest('button');
     const choice = clickedButton.value;
 
@@ -61,12 +78,19 @@ const QuizCategories = () => {
       clickedButton.classList.add('bgRed');
     }
 
-    grabData();
+    socket.emit('submit-answer', {
+      roomid,
+      username,
+      answer: choice,
+      timer: 1500,
+    });
+    // grabData();
   };
 
   return (
     <div className="quiz">
       <h1>Quiz App</h1>
+      <button onClick={connectRoom}>connectRoom</button>
       {data.question ? (
         <CardQuestion category={data.category} question={data.question} />
       ) : (
@@ -82,6 +106,7 @@ const QuizCategories = () => {
                 correct={data.correct_answer}
                 id={index}
                 handleClick={handleClick}
+                // color={buttonColor}
               />
             );
           })}
