18bd89891e4c9345360bac474511ff3cc52f02dc
sam <sampurna.chapagainnn@gmail.com>
2022-08-17 14:44:45 +0100

    add shuffle answers features

--- client/src/Pages/QuizCategories/index.js
+++ client/src/Pages/QuizCategories/index.js
@@ -21,9 +21,30 @@ const QuizCategories = () => {
     const data = await fetchData();
 
     const joinedAnswers = [data.correct_answer, ...data.incorrect_answers];
+    const shuffledAnswers = shuffle(joinedAnswers);
 
     setData(data);
-    setAnswers(joinedAnswers);
+    setAnswers(shuffledAnswers);
+  };
+
+  const shuffle = (array) => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   };
 
   useEffect(() => {
