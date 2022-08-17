dad6280365d4205c242132248b1ce1a33e97768c
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-08-17 14:58:33 +0100

    Accepted yet more incoming changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/about" element={<Pages.About />} />
         <Route path="/quiz" element={<Pages.Quiz />} />
         <Route path="/leaderboard" element={<Pages.LeaderBoard />} />
+        <Route path="/rooms/:roomId" element={<Pages.QuizRoom />} />
       </Routes>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -4,29 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom';
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 
->>>>>>> 0d00b3962d996155fa6a1aaf5c68f8b4423f5719
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <Router>
-     <App />
-  </Router>
-    
-  
-=======
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
->>>>>>> 0d00b3962d996155fa6a1aaf5c68f8b4423f5719
 );
 
 // If you want to start measuring performance in your app, pass a function
