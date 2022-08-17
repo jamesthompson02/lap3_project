326c659794045bae4a7bcab18eadf7e27c09142c
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-08-17 14:38:03 +0100

    Accepting pulled changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
-import Quiz from './Pages/Quiz';
-import QuizRoom from './Pages/QuizRoom';
-import Home from './Pages/Home';
-=======
 import Footer from './Components/Footer';
 import RoutesBar from './Components/RoutesBar';
 import * as Pages from './Pages';
->>>>>>> 0d00b3962d996155fa6a1aaf5c68f8b4423f5719
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <Routes path="/">
-      <Route path="/" element={<Home />} />
-      <Route path="/quiz" element={<Quiz />} />
-      <Route path="/rooms/:roomId" element={<QuizRoom />} />
-
-    </Routes>
-    
-=======
     <div className="App">
       <RoutesBar />
       <Footer />
@@ -32,7 +16,6 @@ const App = () => {
         <Route path="/leaderboard" element={<Pages.LeaderBoard />} />
       </Routes>
     </div>
->>>>>>> 0d00b3962d996155fa6a1aaf5c68f8b4423f5719
   );
 };
 
